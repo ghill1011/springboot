062a80aa00ac1a582cffdf2958c5a7581fdb0937
Gregg <ghill1011@yahoo.com>
2018-02-04 15:20:59 -0600

    put in additional line, prefixed by a tab

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -20,7 +20,8 @@ public class Application {
   @Bean
   public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
     return args -> {
-      System.out.println("Let's inspect the beans provided by Spring Boot:");
+      System.out.println("Let's inspect the beans provided by Spring Boot.");
+      System.out.println("\tHere they are:");
 
       String[] beanNames = ctx.getBeanDefinitionNames();
       Arrays.sort(beanNames);
