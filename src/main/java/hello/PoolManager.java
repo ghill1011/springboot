51ca1cb5ecd435afc64e61b1a8ca820616ac4a98
Gregg <ghill1011@yahoo.com>
2018-03-05 19:34:22 -0600

    fixed some silly bugs

--- src/main/java/hello/PoolManager.java
+++ src/main/java/hello/PoolManager.java
@@ -4,19 +4,19 @@ import java.util.Arrays;
 import redis.clients.jedis.Jedis;
 
 public class PoolManager {
-
-  private static PoolManager instance = null;
+  private JedisPool pool = null;
 
   protected PoolManager() {
     try {
       String vcap_services = System.getenv("VCAP_SERVICES");
-      if (vcap_servcices != null && vcap_servcies.length() > 0) {
+      if (vcap_services != null && vcap_servcies.length() > 0) {
+
         //parsing rediscloud credentials
         JsonRootNode root = new JdomParser().parse(vcap_services);
         JsonNode redisCloudNode = root.getNode("redisCloud");
         JsonNode credentials = root.getNode("credentials");
 
-        JedisPool pool = new JedisPool(new JedisPoolConfig(),
+        pool = new JedisPool(new JedisPoolConfig(),
                 credentials.getStringValue("hostname"),
                 Integer.parseInt(credentials.getNumberValue("port")),
                 Protocol.DEFAULT_TIMEOUT,
@@ -27,7 +27,7 @@ public class PoolManager {
     }
   }
 
-  private static PoolManager getInstance() {
+  public static PoolManager getInstance() {
     if (instance == null) {
       System.out.println("creating new connection manager");
       instance = new PoolManager();
@@ -35,7 +35,8 @@ public class PoolManager {
     return instance;
   }
 
-  public Jedis getJedis() {
-    Jedis jedis = getInstance().getResource();
+  public JedisPool getPool() {
+    return pool;
   }
+
 }
