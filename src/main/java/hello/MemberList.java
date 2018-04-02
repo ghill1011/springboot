8f7f4ceb8668ae987361be04267b833f012d94f7
Gregg <ghill1011@yahoo.com>
2018-04-02 17:34:15 -0500

    first real attempt to use the redis instance

---
+++ src/main/java/hello/MemberList.java
@@ -0,0 +1,63 @@
+package hello;
+
+public class MemberList {
+  private String memberlist;
+  private String partner;
+
+  public MemberList() {
+    // get a connection to persistence
+    PoolManager poolManager = new PoolManager();
+    Jedis jedis = poolManager.getJedis();
+
+    // set the account list
+    jedis.lpush("BankOfAmerica", "1234567890123456789");
+    jedis.lpush("BankOfAmerica", "9123456789012345678");
+    jedis.lpush("BankOfAmerica", "8912345678901234567");
+    jedis.lpush("BankOfAmerica", "7891234567890123456");
+    jedis.lpush("BankOfAmerica", "6789123456789012345");
+    jedis.lpush("BankOfAmerica", "5678912345678901234");
+    jedis.lpush("BankOfAmerica", "4567891234567890123");
+
+    jedis.lpush("Citibank", "1234567890123456789");
+    jedis.lpush("Citibank", "9123456789012345678");
+    jedis.lpush("Citibank", "8912345678901234567");
+    jedis.lpush("Citibank", "7891234567890123456");
+
+    // release the persistence connection
+    poolManager.close();
+  }
+
+  public MemberList(String partner) {
+
+    // get a connection to persistence
+    PoolManager poolManager = new PoolManager();
+    Jedis jedis = poolManager.getJedis();
+
+    // get the account list
+    List<String> list = jedis.lrange(partner+".accounts", 0, jedis.llen());
+    ml = "[";
+    for (String item:list) {
+      ml += item+",";
+    }
+    ml = ml.replaceAll(",+$",""); //trim trailing ","
+    ml +="]";
+    setMemberList(ml);
+
+    // release the persistence connection
+    poolManager.close();
+  }
+
+  public void setMemberList(String memberlist) {
+    this.memberlist = memberlist;
+  }
+  public String getMemberList() {
+    return this.memberlist;
+  }
+  public void setPartner(String partner) {
+    this.partner = partner;
+  }
+  public String getPartner() {
+    return this.partner;
+  }
+
+}
