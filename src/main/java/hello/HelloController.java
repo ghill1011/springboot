7288e24f7144ab8c698783d7d9af49cb74fb081b
Gregg <ghill1011@yahoo.com>
2018-04-15 17:14:10 -0500

    bug fix

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -3,6 +3,7 @@ package hello;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class HelloController {
